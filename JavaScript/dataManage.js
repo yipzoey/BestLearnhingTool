c9a23b964d29a9b63a428aff015ca6971df502b3
yipzoey <106409426+yipzoey@users.noreply.github.com>
2022-07-26 01:06:53 -0400

    Update dataManage.js

--- JavaScript/dataManage.js
+++ JavaScript/dataManage.js
@@ -1,21 +1,16 @@
-let myFlashcards = [ {
-    "question": "Name this species of bird.",
-    "image" : "images/barn-owl-2550068__340.webp",
-    "answer": "Barn Owl"
-    },
-    {
-    "question": "Name this species of bird.",
-    "image" : "images/dove-2516641__340.webp",
-    "answer": "Dove"
-    },
-    {
-    "question": "Name this species of bird.",
-    "image" : "images/hummingbird-2139279__340.webp",
-    "answer": "Hummingbird"
-    }
-  ];
+var quesV,ansV;
+function Reday (){
+  quesV = document.getElementById('quesbox').value;
+  ansV = document.getElementById('answerbox').value;
+}
 
-let dbLocation = firebase.database().ref('deck/flashcards'); 
- 
-dbLocation.set(myFlashcards);
+document.getElementById('update').onclick = function(){
+  Ready();
+
+  firebase.database().ref('deck/flashcards').set({
+    Question:quesV,
+    Answer:ansV,
+  });
+}
+
 
