b724325cfe9086856f25679fdab4b84f28581408
yipzoey <106409426+yipzoey@users.noreply.github.com>
2022-07-25 23:49:30 -0400

    Update dataManage.js

--- JavaScript/dataManage.js
+++ JavaScript/dataManage.js
@@ -1,26 +1,14 @@
-// Make the database point to the location root -> deck -> flashcards
-// If the location doesn't exist is will be created
-// Create the reference location
-let myFlashcards = [ {
-    "question": "Name this species of bird.",
-    "image" : "images/barn-owl-2550068__340.webp",
-    "answer": "Barn Owl"
-    },
-    {
-    "question": "Name this species of bird.",
-    "image" : "images/dove-2516641__340.webp",
-    "answer": "Dove"
-    },
-    {
-    "question": "Name this species of bird.",
-    "image" : "images/hummingbird-2139279__340.webp",
-    "answer": "Hummingbird"
-    }
-  ];
+var quesV,ansV;
+function Reday (){
+  quesV = document.getElementById('quesbox').value;
+  ansV = document.getElementById('answerbox').value;
+}
 
-let dbLocation = firebase.database().ref('deck/flashcards');
+document.getElementById('update').onclick = function(){
+  Ready();
 
-// myFlashcards will be stored under flashcards in the database
-// Anything that was in this location will be overwritten
-// Thus, a write operation also does an update
-dbLocation.set(myFlashcards);
+  firebase.database().ref('deck/flashcards').set({
+    Question:quesV,
+    Answer:ansV,
+  });
+}
